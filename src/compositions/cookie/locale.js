349e324a29e614a0f96deb90c8d5b46da3e4f2fe
亞倫 <tw0905.aaron@gmail.com>
2023-12-22 14:50:07 +0800

    修正 # cookieLocale ctx 判斷錯誤

--- src/compositions/cookie/locale.js
+++ src/compositions/cookie/locale.js
@@ -7,11 +7,12 @@ import { computed, useSSRContext } from 'vue'
  * @param {Object} ssrContext
  * @returns { Object: { locale:ComputedRef } }
  */
-export function useCookieLocale({ ctx: ssrContext = {}, default: defaultValue = 'en' } = {}) {
+export function useCookieLocale({ ctx: ssrContext = null, default: defaultValue = 'en' } = {}) {
   /**
    * @description SSR 時，如果是entry-server，則直接從參數取得 SSR context，否則從 useSSRContext 取得 SSR context
    */
-  const ctx = ssrContext || useSSRContext()
+  let ctx = null
+  if (import.meta.env.SSR) ctx = ssrContext ?? useSSRContext()
 
   if (import.meta.env.SSR && !ctx) {
     throw new Error('useCookieLocale: SSR context is required if in SSR mode.')
@@ -25,7 +26,14 @@ export function useCookieLocale({ ctx: ssrContext = {}, default: defaultValue = 
     set: (value) => cookies.set(COOKIE_KEY.LOCALE, value, { path }),
   })
 
-  if (!locale.value) locale.value = defaultValue
+  if (!locale.value) {
+    locale.value = defaultValue
+    console.warn(
+      `[useCookieLocale]Failed to load cookie locale, set the default locale value [${locale.value}] to application`,
+    )
+  } else {
+    console.log(`[useCookieLocale]Cookie locale is loaded, set the locale [${locale.value}] to application`)
+  }
 
   return {
     locale,
