59315ca072b2d382a4522da9e58c87705a2d44c8
Zizi <54ziziyo@gmail.com>
2023-12-28 10:18:31 +0800

    新增 # 下滑 transition

--- src/components/common/Accordion.vue
+++ src/components/common/Accordion.vue
@@ -5,31 +5,30 @@
       @click="toggleSection(index)"
     >
       <div class="text-gray-700">{{ section.title }}</div>
-      <Icon
-        name="back"
-        size="12"
-        class="rotate-[270deg] transition-transform"
-        :class="{ 'rotate-90': section.open }"
-      ></Icon>
+      <Icon name="dropdown" size="8" class="transition-transform" :class="{ 'rotate-180': section.open }"></Icon>
     </div>
     <div
-      v-if="section.open"
-      class="transition-max-h grid max-h-full gap-y-10 bg-slate-100 px-20 py-10 duration-1000 ease-in-out"
+      class="grid gap-y-10 bg-slate-100 transition-all"
+      :class="[{ 'max-h-[1000px] px-20 py-10': section.open, 'max-h-0': !section.open }]"
     >
       <div v-for="(subItem, subIndex) in section.content" :key="subIndex">
         <div class="flex cursor-pointer justify-between py-4" @click="toggleSubItem(section, subIndex)">
-          <div class="text-[0.875rem] font-normal leading-[1.125rem] text-gray-600">
+          <div class="text-[0.875rem] font-normal leading-[1.125rem] text-gray-600" :class="{ hidden: !section.open }">
             {{ subItem.subTitle }}
           </div>
           <Icon
-            name="back"
-            size="10"
-            class="rotate-[270deg] transition-transform"
-            :class="{ 'rotate-90': subItem.open }"
+            name="dropdown"
+            size="6"
+            class="transition-transform"
+            :class="{ 'rotate-180': subItem.open, hidden: !section.open }"
           ></Icon>
         </div>
-        <div v-if="subItem.open" class="transition-max-h max-h-full px-20 duration-1000 ease-in-out">
-          <span class="leading-[0.75rem text-[0.75rem] font-normal text-gray66">{{ subItem.subContent }}</span>
+        <div class="px-20 transition-all" :class="[{ 'max-h-[1000px]': subItem.open, 'max-h-0': !subItem.open }]">
+          <span
+            class="leading-[0.75rem text-[0.75rem] font-normal text-gray66 transition-all delay-300"
+            :class="{ flex: subItem.open, hidden: !subItem.open }"
+            >{{ subItem.subContent }}</span
+          >
         </div>
       </div>
     </div>
