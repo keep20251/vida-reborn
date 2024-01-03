c3c5d6cadc0aed8cfc3684c366e6921948761b1e
亞倫 <tw0905.aaron@gmail.com>
2024-01-03 18:28:28 +0800

    新增 # 帳戶 & 密碼登入

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -5,16 +5,19 @@
         <div class="flex flex-col gap-y-32">
           <div class="flex flex-col gap-y-8">
             <InputWrap
-              v-model="credential.account"
+              v-model="credential.account.value"
               :label="$t('label.account')"
               :placeholder="$t('placeholder.account')"
+              :err-msg="credential.account.error"
             ></InputWrap>
             <InputWrap
-              v-model="credential.password"
+              v-model="credential.password.value"
               :label="$t('label.password')"
               :placeholder="$t('placeholder.password')"
+              password
+              :err-msg="credential.password.error"
             ></InputWrap>
-            <Button>{{ $t('label.login') }}</Button>
+            <Button :loading="isLoading" @click="validaite">{{ $t('label.login') }}</Button>
           </div>
           <div class="text-center">
             <span>
@@ -30,17 +33,73 @@
   </div>
 </template>
 <script setup>
-import { useAuthRouteStore } from '@/store/auth-route'
 import DialogHeader from '@/components/dialog/DialogHeader.vue'
 import InputWrap from '@/components/form/InputWrap.vue'
 import Button from '@/components/common/Button.vue'
+import useRequest from '@use/request/index.js'
 import { AUTH_ROUTES } from '@/constant'
-import { reactive } from 'vue'
+import { useAuthRouteStore } from '@/store/auth-route'
+import { reactive, ref } from 'vue'
+import { useYup } from '@use/validator/yup.js'
+import { useAccountStore } from '@/store/account'
+import { useRouter } from 'vue-router'
 
-const { to, back } = useAuthRouteStore()
+const { to, back, close } = useAuthRouteStore()
+
+const { Yup, validate } = useYup()
+const { string } = Yup
 
 const credential = reactive({
-  account: '',
-  password: '',
+  account: {
+    value: '',
+    error: '',
+    check: false,
+    schema: string().required(),
+  },
+  password: {
+    value: '',
+    error: '',
+    check: false,
+    schema: string().required(),
+  },
 })
+
+const isLoading = ref(false)
+
+async function validaite() {
+  try {
+    isLoading.value = true
+    await Promise.all([
+      validate(credential.account.schema, credential.account),
+      validate(credential.password.schema, credential.password),
+    ])
+
+    if (Object.values(credential).some((item) => item.check === false)) return
+    login()
+  } catch (e) {
+    console.error(e)
+    isLoading.value = false
+  }
+}
+
+const accountStore = useAccountStore()
+const { setToken } = accountStore
+const router = useRouter()
+
+async function login() {
+  const { data, execute } = useRequest('Account.loginByPassword', {})
+  try {
+    await execute({
+      account: credential.account.value,
+      password: credential.password.value,
+    })
+    setToken(data.value.token)
+    close()
+    router.push({ name: 'home' })
+  } catch (e) {
+    console.error(e)
+  } finally {
+    isLoading.value = false
+  }
+}
 </script>
