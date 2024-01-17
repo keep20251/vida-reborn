5374f57bab047fbcf715275507285a968b5c3e6a
亞倫 <tw0905.aaron@gmail.com>
2024-01-17 14:36:52 +0800

    新增 # Social Link Dialog

---
+++ src/components/modal/content/SocialLink.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex w-full flex-col gap-y-20 px-14 py-30">
+    <div class="flex flex-col gap-y-5">
+      <div class="text-base font-normal leading-3">{{ $t('info.socialLink') }}</div>
+      <div class="flex flex-col gap-y-10">
+        <InputWrap prepend-icon="socialInstagram"></InputWrap>
+        <InputWrap prepend-icon="socialFacebook"></InputWrap>
+        <InputWrap prepend-icon="socialYoutube"></InputWrap>
+        <InputWrap prepend-icon="socialTwitter"></InputWrap>
+        <InputWrap prepend-icon="socialTiktok"></InputWrap>
+      </div>
+    </div>
+    <div class="flex flex-col gap-y-10">
+      <div class="text-base font-normal leading-3">{{ $t('info.forExample') }}:https://www.instagram.com/username</div>
+      <div class="text-base font-normal leading-3">
+        {{ $t('info.accept') }}:Instagram \ Facebook \ Youtube \ Twitter \ Tiktok
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import InputWrap from '@/components/form/InputWrap.vue'
+import { useConfirmData } from '@/compositions/modal/confirm-data'
+
+useConfirmData(() => {
+  console.log('useConfirmData')
+  return true
+})
+</script>
--- src/components/modal/content/index.js
+++ src/components/modal/content/index.js
@@ -1,11 +1,12 @@
 import Text from './Text.vue'
 import Test from './Test.vue'
+import SocialLink from '@/components/modal/content/SocialLink.vue'
 
 import { MODAL_TYPE } from '@/constant'
 
 export default Object.freeze({
   [MODAL_TYPE.ALERT]: Text,
   [MODAL_TYPE.CONFIRM]: Text,
-
+  [MODAL_TYPE.SOCIAL_LINK]: SocialLink,
   [MODAL_TYPE.TEST]: Test,
 })
