56416d49b7c8428169f2710c00be652cd4564acd
亞倫 <tw0905.aaron@gmail.com>
2024-01-19 18:31:30 +0800

    新增 # multi-auth 用於集成有關帳號驗證相關的函式，包括電子郵件驗證碼發送

---
+++ src/compositions/request/multi-auth.js
@@ -0,0 +1,28 @@
+import useRequest from './index'
+import { SEND_EMAIL_PURPOSE } from '@/constant'
+
+/**
+ * 因目前Vida的登入方式有很多種，
+ * 有的是用帳號密碼，有的是用手機號碼，有的是用Email，
+ * 電子郵件的驗證碼用途也很多，
+ * 所以這裡統一管理會用到的一些方法，
+ */
+export function useMultiAuth() {
+  /**
+   * 發送電子郵件驗證碼
+   * type:
+   *  1 綁定手機
+   *  2 解綁手機
+   *  3 找回帳號
+   *  4 驗證信箱
+   *  5 快速登入
+   */
+  async function sendEmailCode({ email, type = SEND_EMAIL_PURPOSE.FAST_LOGIN } = {}) {
+    const { execute, data } = useRequest('Account.sendEmailCode')
+    await execute({ email, type })
+    console.log('sendEmailCode.data', data)
+    return data
+  }
+
+  return { sendEmailCode }
+}
--- src/store/email-login.js
+++ src/store/email-login.js
@@ -1,22 +1,14 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
-import useRequest from '@/compositions/request'
-import { SEND_EMAIL_PURPOSE } from '@/constant'
 
 export const useEmailLoginStore = defineStore('email-login-store', () => {
   const email = ref('')
+  const verifyCode = ref('')
   const password = ref('')
-
-  async function sendEmailCode(type = SEND_EMAIL_PURPOSE.FAST_LOGIN) {
-    const { execute, data } = useRequest('Account.sendEmailCode')
-    // TODO sendEmailCode 需要帶入 account ，這個需求還不明確，之後再回來處理
-    await execute({ email: email.value, type, account: 'test-user' })
-    return data
-  }
 
   return {
     email,
     password,
-    sendEmailCode,
+    verifyCode,
   }
 })
