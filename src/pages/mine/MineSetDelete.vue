38453ab915944de17c8e821bcb78c6293b9397c5
Zizi <54ziziyo@gmail.com>
2024-01-24 18:59:32 +0800

    新增 # 刪除帳號 串接

--- src/pages/mine/MineSetDelete.vue
+++ src/pages/mine/MineSetDelete.vue
@@ -3,19 +3,42 @@
     <div class="space-y-20">
       <div class="leading-lg mb-10 text-base font-normal text-gray66">{{ $t('content.delAccContent') }} example@mail.com</div>
       <InputWrap
-        v-model="inputValue"
+        v-model="nowPw"
         :label="$t('label.nowPw')"
         :placeholder="$t('placeholder.nowPw')"
         password
       ></InputWrap>
-      <Button @click="onBtnClick('contrast button')">{{ $t('content.delAcc') }}</Button>
+      <Button @click="deleteAcc">{{ $t('content.delAcc') }}</Button>
+      <div v-if="!!serverError" class="text-sm font-normal leading-md text-warning">{{ serverError }}</div>
     </div>
   </div>
 </template>
 <script setup>
 import { ref } from 'vue'
+import { useAccountStore } from '@/store/account'
 import Button from '@comp/common/Button.vue'
 import InputWrap from '@comp/form/InputWrap.vue'
+import useRequest from '@use/request/index.js'
 
-const inputValue = ref('')
+const accountStore = useAccountStore()
+const { logout } = accountStore
+
+const nowPw = ref('')
+
+const serverError = ref('')
+async function deleteAcc() {
+  const { execute } = useRequest('User.deleteAccount')
+  try{
+    await execute({
+      old_password: nowPw.value
+    })
+    console.log('成功囉！')
+        setTimeout(() => {
+      logout()
+    }, 500);
+  } catch (e) {
+    serverError.value = e.message
+    console.error(e)
+  }
+}
 </script>
