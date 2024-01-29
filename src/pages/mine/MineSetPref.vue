f3fdc8bdda7b57ae750ae0b78f6ff4b2943422e7
Zizi <54ziziyo@gmail.com>
2024-01-29 18:29:48 +0800

    調整 # 使用 appStore 的 categories

--- src/pages/mine/MineSetPref.vue
+++ src/pages/mine/MineSetPref.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="border-t pt-20">
     <label class="text-base font-normal not-italic leading-md">{{ $t('title.prefer') }}： </label>
-    <OptionsPicker v-model="interested" :options="options" class="!justify-start"></OptionsPicker>
+    <OptionsPicker v-model="interested" :options="categories" class="!justify-start"></OptionsPicker>
     <div class="py-20 text-sm font-normal leading-3 text-gray-57">{{ $t('label.multiple') }}</div>
     <Button @click="savePref">{{ $t('common.save') }}</Button>
     <div v-if="!!serverError" class="text-sm font-normal leading-md text-warning">{{ serverError }}</div>
@@ -10,15 +10,16 @@
 
 <script setup>
 import { onMounted, ref } from 'vue'
-import { useI18n } from 'vue-i18n'
 import { storeToRefs } from 'pinia'
 import { useAccountStore } from '@/store/account'
+import { useAppStore } from '@/store/app'
 import { useMineStore } from '@/store/mine'
 import Button from '@comp/common/Button.vue'
 import OptionsPicker from '@comp/form/OptionsPicker.vue'
 import useRequest from '@use/request/index.js'
 
-const { t: $t } = useI18n()
+const appStore = useAppStore()
+const { categories } = storeToRefs(appStore)
 
 const accountStore = useAccountStore()
 const { userData } = accountStore
@@ -26,29 +27,11 @@ const { userData } = accountStore
 const mineStore = useMineStore()
 const { interested } = storeToRefs(mineStore)
 
-const options = ref([])
-const serverError = ref('')
-
 onMounted(() => {
-  interested.value = userData.interested.split(',').map(Number) // 字串轉成陣列
-  data()
+  interested.value = userData.interested.split(',').map(String) // 字串轉成陣列
 })
 
-const data = async () => {
-  let categories
-  try {
-    categories = await useRequest('Article.categories', { immediate: true })
-
-    // 將後端取得的 categories.list 轉換為 options 格式
-    options.value = Object.entries(categories.list).map(([value, label]) => ({
-      value: parseInt(value, 10), // 確保解析的值是十進制
-      label: $t(`category.${value}`),
-    }))
-  } catch (e) {
-    console.error(e)
-  }
-}
-
+const serverError = ref('')
 async function savePref() {
   const { execute } = useRequest('User.modifyInfo')
   try {
