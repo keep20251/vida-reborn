88634c1ad645a5942caae233c15e0f7eb62a06ca
LuLu <hubert.lulululu@gmail.com>
2024-01-30 10:43:35 +0800

    調整 # useInfinite 調整完成

--- src/compositions/request/infinite.js
+++ src/compositions/request/infinite.js
@@ -11,70 +11,70 @@ export function useInfinite(apiKey, { params = {}, limit = 10 } = {}) {
   // 請求資料清單
   const dataList = ref([])
 
-  // 是否正在請求中
-  const isLoading = ref(false)
-
   // 沒有更多了，當判斷到回傳資料數量小於 limit 就會被判定為沒有更多資料
   const noMore = ref(false)
 
-  // 任何請求時捕捉到的錯誤訊息
-  const errorMsg = ref('')
-
-  const { data, execute } = useRequest(apiKey)
+  const { data, error, isLoading, execute, cancel } = useRequest(apiKey)
 
   async function init() {
     if (!isLoading.value && !noMore.value && dataList.value.length === 0) {
-      await nextPage()
+      return await next()
     }
+    return Promise.reject(new Error('Infinite already init...'))
   }
 
-  async function refresh({ newParams = params, newLimit = limit } = {}) {
+  function reset({ newParams = params, newLimit = limit } = {}) {
+    cancel()
     params = newParams
     limit = newLimit
-
-    isLoading.value = false
-    noMore.value = false
-    errorMsg.value = ''
     dataList.value = []
-    await nextPage()
+    noMore.value = false
   }
 
-  async function nextPage() {
+  async function next() {
     if (isLoading.value) {
-      console.warn('Infinite reqeust is loading, but you still call nextPage() to request.')
-      return
+      console.warn('Infinite reqeust is loading, but you still call next() to request.')
+      return Promise.reject(new Error('Infinite executor is loading...'))
     }
     if (noMore.value) {
-      console.warn('Infinite reqeust is no more data, but you still call nextPage() to request.')
-      return
+      console.warn('Infinite reqeust is no more data, but you still call next() to request.')
+      return Promise.reject(new Error('Infinite is no more data...'))
     }
 
-    errorMsg.value = ''
-    isLoading.value = true
     const page = Math.ceil(dataList.value.length / limit) + 1
 
     try {
       await execute({ page, limit, ...params })
 
-      if (data.value.length < limit) {
+      if (!Array.isArray(data.value.list)) {
+        throw new Error(`API ${apiKey} response data.list is not array...`)
+      }
+
+      const newDataList = [...data.value.list]
+      if (newDataList.length < limit) {
         noMore.value = true
       }
-      dataList.value.push(...data.value)
+      dataList.value.push(...newDataList)
+      return newDataList
     } catch (e) {
-      console.warn(e)
-      errorMsg.value = e.message
-    } finally {
-      isLoading.value = false
+      console.error(e)
+      return Promise.reject(e)
     }
   }
 
+  async function reload({ newParams = params, newLimit = limit } = {}) {
+    reset({ newParams, newLimit })
+    return await init()
+  }
+
   return {
-    dataList,
-    isLoading: readonly(isLoading),
+    dataList: readonly(dataList),
+    error,
+    isLoading,
     noMore: readonly(noMore),
-    errorMsg: readonly(errorMsg),
     init,
-    refresh,
-    nextPage,
+    reset,
+    next,
+    reload,
   }
 }
