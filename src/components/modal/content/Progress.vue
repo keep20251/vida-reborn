36ceac4d9e53d3de53b0db9364101e6871446403
亞倫 <tw0905.aaron@gmail.com>
2024-02-01 17:10:19 +0800

    新增 # 上傳進度Dialog

---
+++ src/components/modal/content/Progress.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <ProgressBar :model-value="content?.progress" size="lg"></ProgressBar>
+  </div>
+</template>
+<script setup>
+import { storeToRefs } from 'pinia'
+import { useModalStore } from '@/store/modal'
+import ProgressBar from '@comp/progress/ProgressBar.vue'
+
+const { content } = storeToRefs(useModalStore())
+</script>
---
+++ src/components/progress/ProgressBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="h-8 w-full rounded-full bg-slate-600">
+    <div class="h-8 rounded-full bg-primary" :class="{ height }" :style="{ width }"></div>
+  </div>
+</template>
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  modelValue: { type: Number, default: 0 },
+  size: { type: String, default: 'lg' }, // lg | md | sm
+})
+
+const height = computed(() => {
+  switch (props.size) {
+    case 'md':
+      return 'h-8'
+    case 'sm':
+      return 'h-6'
+    case 'lg':
+    default:
+      return 'h-10'
+  }
+})
+
+const width = computed(() => props.modelValue + '%')
+</script>
