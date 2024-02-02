9fd69be8e993fe6522c2d4a560da65ca687e2d1e
LuLu <hubert.lulululu@gmail.com>
2024-02-02 14:16:00 +0800

    調整 # toCreator 自己導去 mine, 他人導去 creator

--- src/compositions/routers/index.js
+++ src/compositions/routers/index.js
@@ -1,7 +1,11 @@
 import { useRouter } from 'vue-router'
+import { storeToRefs } from 'pinia'
+import { useAccountStore } from '@/store/account'
 
 export function useRouters() {
   const router = useRouter()
+  const accountStore = useAccountStore()
+  const { username: $username } = storeToRefs(accountStore)
 
   function back() {
     if (window.history.state.back === null) {
@@ -13,10 +17,19 @@ export function useRouters() {
   }
 
   function to(name, { params, query } = {}) {
+    // mine 頁用 push 物件的方式導過去必須使用 child 的第一個名稱才會顯示出來
+    // 感覺是 vue-router 的坑
+    if (name === 'mine') {
+      name = 'mine-home'
+    }
+
     return router.push({ name, params, query })
   }
 
   function toCreator(username) {
+    if (username === $username.value) {
+      return to('mine')
+    }
     return to('creator', { params: { username } })
   }
 
