09717b64e7ef0c99ca5d95a597d59df3b073f7ae
Zizi <54ziziyo@gmail.com>
2024-02-05 14:04:56 +0800

    新增 # 我的主頁 icon

--- src/components/mine/SetList.vue
+++ src/components/mine/SetList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex select-none flex-col divide-y">
-    <div v-if="perm.registerLogin" class="grid space-y-10 py-10">
+    <!-- <div v-if="perm.registerLogin" class="grid space-y-10 py-10">
       <div class="flex cursor-pointer items-center space-x-18" @click="openAuthDialog(AUTH_ROUTES.SIGN_UP)">
         <Icon name="setting" size="20"></Icon>
         <span>{{ $t('title.join') }}</span>
@@ -9,37 +9,37 @@
         <Icon name="setting" size="20"></Icon>
         <span>{{ $t('title.login') }}</span>
       </div>
-    </div>
+    </div> -->
     <div v-if="perm.postEarn" class="grid space-y-10 py-10">
       <router-link :to="{ name: 'mine-post' }" class="flex cursor-pointer items-center space-x-18">
-        <Icon name="setting" size="20"></Icon>
+        <Icon name="minePost" size="20"></Icon>
         <span>{{ $t('title.post') }}</span>
       </router-link>
       <router-link :to="{ name: 'mine-earn' }" class="flex cursor-pointer items-center space-x-18">
-        <Icon name="setting" size="20"></Icon>
+        <Icon name="mineEarn" size="20"></Icon>
         <span>{{ $t('title.earn') }}</span>
       </router-link>
     </div>
     <div v-if="perm.buyCollect" class="grid space-y-10 py-10">
       <router-link :to="{ name: 'mine-buy' }" class="flex cursor-pointer items-center space-x-18">
-        <Icon name="setting" size="20"></Icon>
+        <Icon name="mineBuy" size="20"></Icon>
         <span>{{ $t('title.buy') }}</span>
       </router-link>
       <router-link :to="{ name: 'mine-collect' }" class="flex cursor-pointer items-center space-x-18">
-        <Icon name="setting" size="20"></Icon>
+        <Icon name="mineCollect" size="20"></Icon>
         <span>{{ $t('title.collect') }}</span>
       </router-link>
     </div>
     <div v-if="perm.beCreator" class="grid space-y-10 py-10">
       <router-link :to="`/${locale}/mine/creator`" class="flex cursor-pointer items-center space-x-18">
-        <Icon name="setting" size="20"></Icon>
+        <Icon name="mineBeCreator" size="20"></Icon>
         <span>{{ $t('title.beCreator') }}</span>
       </router-link>
     </div>
     <div v-if="perm.settings" class="grid space-y-10 py-10">
       <div class="flex cursor-pointer items-center justify-between pr-15" @click="accOpen = !accOpen">
         <div class="flex items-center space-x-18">
-          <Icon name="setting" size="20"></Icon>
+          <Icon name="mineSet" size="20"></Icon>
           <span>{{ $t('title.setting') }}</span>
         </div>
         <Icon name="dropdown" size="8" class="transition-transform" :class="{ 'rotate-180': accOpen === true }"></Icon>
@@ -47,26 +47,26 @@
       <div class="pl-20 transition-all" :class="{ hidden: !accOpen }">
         <div class="flex w-full flex-col space-y-10 transition-all delay-1000" :class="{ hidden: !accOpen }">
           <router-link :to="{ name: 'mine-account' }" class="flex cursor-pointer items-center space-x-18">
-            <Icon name="setting" size="20"></Icon><span>{{ $t('title.usnMail') }}</span>
+            <Icon name="mineSetAcc" size="20"></Icon><span>{{ $t('title.usnMail') }}</span>
           </router-link>
           <router-link :to="{ name: 'mine-password' }" class="flex cursor-pointer items-center space-x-18">
-            <Icon name="setting" size="20"></Icon><span>{{ $t('title.password') }}</span>
+            <Icon name="mineSetPw" size="20"></Icon><span>{{ $t('title.password') }}</span>
           </router-link>
           <router-link :to="{ name: 'mine-preference' }" class="flex cursor-pointer items-center space-x-18">
-            <Icon name="setting" size="20"></Icon><span>{{ $t('title.prefer') }}</span>
+            <Icon name="mineSetPrefer" size="20"></Icon><span>{{ $t('title.prefer') }}</span>
           </router-link>
           <router-link :to="{ name: 'mine-block' }" class="flex cursor-pointer items-center space-x-18">
-            <Icon name="setting" size="20"></Icon><span>{{ $t('title.blockAcc') }}</span>
+            <Icon name="mineSetBlock" size="20"></Icon><span>{{ $t('title.blockAcc') }}</span>
           </router-link>
           <router-link :to="{ name: 'mine-delete' }" class="flex cursor-pointer items-center space-x-18">
-            <Icon name="setting" size="20"></Icon><span>{{ $t('title.delAcc') }}</span>
+            <Icon name="mineSetDelAcc" size="20"></Icon><span>{{ $t('title.delAcc') }}</span>
           </router-link>
         </div>
       </div>
     </div>
     <div class="flex justify-between space-y-5 py-10">
       <div class="flex items-center space-x-18">
-        <Icon name="setting" size="20"></Icon>
+        <Icon name="mineLang" size="20"></Icon>
         <span>{{ $t('title.language') }}</span>
       </div>
       <Dropdown class="w-[120px]" v-model="locale" :options="transOptions"></Dropdown>
@@ -74,7 +74,7 @@
     <div class="grid space-y-10 py-10">
       <div class="flex cursor-pointer items-center justify-between pr-15" @click="aboutOpen = !aboutOpen">
         <div class="flex items-center space-x-18">
-          <Icon name="setting" size="20"></Icon>
+          <Icon name="mineAbout" size="20"></Icon>
           <span>{{ $t('title.about') }}</span>
         </div>
         <Icon
@@ -87,25 +87,27 @@
       <div class="pl-20 transition-all" :class="{ hidden: !aboutOpen }">
         <div class="flex w-full flex-col space-y-10 transition-all delay-1000" :class="{ hidden: !aboutOpen }">
           <router-link :to="{ name: 'mine-tos' }" class="flex cursor-pointer items-center space-x-18">
-            <Icon name="setting" size="20"></Icon><span>{{ $t('title.tos') }}</span>
+            <Icon name="mineAboutTos" size="20"></Icon><span>{{ $t('title.tos') }}</span>
           </router-link>
           <router-link :to="{ name: 'mine-pp' }" class="flex cursor-pointer items-center space-x-18">
-            <Icon name="setting" size="20"></Icon><span>{{ $t('title.pp') }}</span>
+            <Icon name="mineAboutPp" size="20"></Icon><span>{{ $t('title.pp') }}</span>
           </router-link>
           <router-link :to="{ name: 'mine-cp' }" class="flex cursor-pointer items-center space-x-18">
-            <Icon name="setting" size="20"></Icon><span>{{ $t('title.cp') }}</span>
+            <Icon name="mineAboutCp" size="20"></Icon><span>{{ $t('title.cp') }}</span>
           </router-link>
           <router-link :to="{ name: 'mine-dmca' }" class="flex cursor-pointer items-center space-x-18">
-            <Icon name="setting" size="20"></Icon><span>DMCA</span>
+            <Icon name="mineAboutDmca" size="20"></Icon><span>DMCA</span>
           </router-link>
-          <div class="flex items-center space-x-5"><Icon name="setting" size="20"></Icon><span>V1.0.0</span></div>
+          <div class="items-center space-x-18 flex">
+            <Icon name="mineAboutVersion" size="20"></Icon><span>V1.0.0</span>
+          </div>
         </div>
       </div>
     </div>
 
     <div v-if="perm.logout" class="grid space-y-10 py-10">
       <div class="flex cursor-pointer items-center space-x-18" @click="logout">
-        <Icon name="setting" size="20"></Icon>
+        <Icon name="mineLogout" size="20"></Icon>
         <span>{{ $t('title.logout') }}</span>
       </div>
     </div>
