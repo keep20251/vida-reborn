c054e5c8ef030546000efd456f1ce0968013dab0
Zizi <54ziziyo@gmail.com>
2024-02-06 10:04:33 +0800

    調整 # 關於條款

--- src/pages/mine/MineAboutCP.vue
+++ src/pages/mine/MineAboutCP.vue
@@ -1,57 +1,57 @@
 <template>
   <div>
     <div class="space-y-5">
-      <div class="text-sm font-medium leading-md">{{ i18n.cp?.info.title }}</div>
-      <div class="text-xs font-normal leading-md">{{ i18n.cp?.info.content }}</div>
-      <div class="pt-2 text-sm font-medium leading-md">{{ i18n.cp?.definitions.title }}</div>
+      <div class="text-base font-medium leading-md">{{ i18n.cp?.info.title }}</div>
+      <div class="text-sm font-normal leading-md">{{ i18n.cp?.info.content }}</div>
+      <div class="pt-2 text-base font-medium leading-md">{{ i18n.cp?.definitions.title }}</div>
       <div>
-        <div class="text-xs font-normal leading-md">
+        <div class="text-sm font-normal leading-md">
           {{ i18n.cp?.definitions.table.title.b }}：{{ i18n.cp?.definitions.table.content.b }}
         </div>
-        <div class="text-xs font-normal leading-md">
+        <div class="text-sm font-normal leading-md">
           {{ i18n.cp?.definitions.table.title.c }}：{{ i18n.cp?.definitions.table.content.c }}
         </div>
-        <div class="text-xs font-normal leading-md">
+        <div class="text-sm font-normal leading-md">
           {{ i18n.cp?.definitions.table.title.d }}：{{ i18n.cp?.definitions.table.content.d }}
         </div>
-        <div class="text-xs font-normal leading-md">
+        <div class="text-sm font-normal leading-md">
           {{ i18n.cp?.definitions.table.title.e }}：{{ i18n.cp?.definitions.table.content.e }}
         </div>
       </div>
-      <div class="pt-2 text-sm font-medium leading-md">{{ i18n.cp?.modifications.title }}</div>
-      <div class="text-xs font-normal leading-md">{{ i18n.cp?.modifications.content }}</div>
-      <div class="pt-2 text-sm font-medium leading-md">{{ i18n.cp?.useCookie.title }}</div>
-      <div class="text-xs font-normal leading-md">{{ i18n.cp?.useCookie.content1 }}</div>
+      <div class="pt-2 text-base font-medium leading-md">{{ i18n.cp?.modifications.title }}</div>
+      <div class="text-sm font-normal leading-md">{{ i18n.cp?.modifications.content }}</div>
+      <div class="pt-2 text-base font-medium leading-md">{{ i18n.cp?.useCookie.title }}</div>
+      <div class="text-sm font-normal leading-md">{{ i18n.cp?.useCookie.content1 }}</div>
       <div>
-        <div class="text-xs font-normal leading-md">
+        <div class="text-sm font-normal leading-md">
           {{ i18n.cp?.useCookie.table.title.a }}：{{ i18n.cp?.useCookie.table.content.a }}
         </div>
-        <div class="text-xs font-normal leading-md">
+        <div class="text-sm font-normal leading-md">
           {{ i18n.cp?.definitions.table.title.b }}：{{ i18n.cp?.definitions.table.content.b }}
         </div>
-        <div class="text-xs font-normal leading-md">
+        <div class="text-sm font-normal leading-md">
           {{ i18n.cp?.definitions.table.title.c }}：{{ i18n.cp?.definitions.table.content.c }}
         </div>
-        <div class="text-xs font-normal leading-md">
+        <div class="text-sm font-normal leading-md">
           {{ i18n.cp?.definitions.table.title.d }}：{{ i18n.cp?.definitions.table.content.d }}
         </div>
-        <div class="text-xs font-normal leading-md">
+        <div class="text-sm font-normal leading-md">
           {{ i18n.cp?.definitions.table.title.e }}：{{ i18n.cp?.definitions.table.content.e }}
         </div>
       </div>
-      <div class="text-xs font-normal leading-md">{{ i18n.cp?.useCookie.content2 }}</div>
-      <div class="pt-2 text-sm font-medium leading-md">{{ i18n.cp?.analyticsAd.title }}</div>
-      <div class="text-xs font-normal leading-md">{{ i18n.cp?.analyticsAd.content }}</div>
-      <div class="pt-2 text-sm font-medium leading-md">{{ i18n.cp?.choices.title }}</div>
-      <div class="text-xs font-normal leading-md">{{ i18n.cp?.choices.content }}</div>
-      <div class="pt-2 text-sm font-medium leading-md">{{ i18n.cp?.contact.title }}</div>
-      <div class="text-xs font-normal leading-md">{{ i18n.cp?.contact.content }}</div>
-      <div class="pt-2 text-sm font-medium leading-md">{{ i18n.cp?.miscellaneous.title }}</div>
-      <div class="text-xs font-normal leading-md">{{ i18n.cp?.miscellaneous.content }}</div>
-      <div class="pt-2 text-sm font-medium leading-md">{{ i18n.cp?.language.title }}</div>
-      <div class="text-xs font-normal leading-md">{{ i18n.cp?.language.content }}</div>
-      <div class="pt-2 text-xs font-normal leading-md">{{ i18n.cp?.final }}</div>
-      <div class="pt-2 text-xs font-normal leading-md">{{ i18n.cp?.fantasi }}</div>
+      <div class="text-sm font-normal leading-md">{{ i18n.cp?.useCookie.content2 }}</div>
+      <div class="pt-2 text-base font-medium leading-md">{{ i18n.cp?.analyticsAd.title }}</div>
+      <div class="text-sm font-normal leading-md">{{ i18n.cp?.analyticsAd.content }}</div>
+      <div class="pt-2 text-base font-medium leading-md">{{ i18n.cp?.choices.title }}</div>
+      <div class="text-sm font-normal leading-md">{{ i18n.cp?.choices.content }}</div>
+      <div class="pt-2 text-base font-medium leading-md">{{ i18n.cp?.contact.title }}</div>
+      <div class="text-sm font-normal leading-md">{{ i18n.cp?.contact.content }}</div>
+      <div class="pt-2 text-base font-medium leading-md">{{ i18n.cp?.miscellaneous.title }}</div>
+      <div class="text-sm font-normal leading-md">{{ i18n.cp?.miscellaneous.content }}</div>
+      <div class="pt-2 text-base font-medium leading-md">{{ i18n.cp?.language.title }}</div>
+      <div class="text-sm font-normal leading-md">{{ i18n.cp?.language.content }}</div>
+      <div class="pt-2 text-sm font-normal leading-md">{{ i18n.cp?.final }}</div>
+      <div class="pt-2 text-sm font-normal leading-md">{{ i18n.cp?.fantasi }}</div>
     </div>
   </div>
 </template>
