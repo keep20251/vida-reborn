65538e548015f1a92d2e76feec3f8d0d1c8b8e8e
Zizi <54ziziyo@gmail.com>
2024-02-17 19:28:59 +0800

    新增 # 建立訂閱方案串接

--- src/store/sub-plan.js
+++ src/store/sub-plan.js
@@ -5,7 +5,6 @@ import MainPage from '@comp/subPlan/MainPage.vue'
 import SubPlanSet from '@comp/subPlan/SubPlanSet.vue'
 import { SUB_PLAN } from '@const'
 
-
 export const useSubPlanStore = defineStore('subPlan', () => {
   const { subPlanDialog } = storeToRefs(useDialogStore())
 
@@ -18,16 +17,24 @@ export const useSubPlanStore = defineStore('subPlan', () => {
   const history = ref([])
   const historyProxy = computed(() => history.value)
   const subPlanComponent = computed(() => routes.find((route) => route.value === now.value).component)
-  
+
   const addSubPlan = ref(false)
   const data = ref('')
   const index = ref('')
-  const subPlanName = ref('')  // 必傳
+  const subPlanName = ref('') // 必傳
   const subPlanContent = ref('')
-  const subPlanPrice = ref('')  // 必傳
-  const subUnlockDayAfter = ref('')// 必傳(解鎖Ｎ天前的內榮)
-  const subId = ref('')  // 必傳
+  const subPlanPrice = ref('') // 必傳
+  const subUnlockDayAfter = ref('') // 必傳(解鎖Ｎ天前的內榮)
+  const subId = ref('') // 必傳
   const subPicture = ref('')
+  const uploadFiles = ref([])
+
+  // 等待 UI 圖片整理完成
+  const defaultImg = ref([
+    'https://images.pexels.com/photos/764368/pexels-photo-764368.jpeg',
+    'https://images.pexels.com/photos/3782766/pexels-photo-3782766.jpeg',
+    'https://images.pexels.com/photos/3226837/pexels-photo-3226837.jpeg',
+  ])
 
   function to(value) {
     history.value.push(now.value)
@@ -38,13 +45,16 @@ export const useSubPlanStore = defineStore('subPlan', () => {
     if (history.value.length <= 0) {
       throw new Error('[Auth Route Error] History is empty, you shoud not call back()')
     }
-   
+
+    console.log(subPicture.value, '你是說少')
+    uploadFiles.value = []
     now.value = history.value.pop()
   }
 
   function close() {
     now.value = SUB_PLAN.MAIN_PAGE
     subPlanDialog.value = false
+    uploadFiles.value = []
     history.value = []
   }
 
@@ -53,17 +63,15 @@ export const useSubPlanStore = defineStore('subPlan', () => {
     subPlanDialog.value = true
   }
 
-  
-
-  return { 
-    subPlanComponent, 
-    history: historyProxy, 
-    to, 
+  return {
+    subPlanComponent,
+    history: historyProxy,
+    to,
     back,
-    close, 
-    open, 
+    close,
+    open,
     addSubPlan,
-    data, 
+    data,
     index,
     subPlanName,
     subPlanContent,
@@ -71,5 +79,7 @@ export const useSubPlanStore = defineStore('subPlan', () => {
     subUnlockDayAfter,
     subId,
     subPicture,
+    uploadFiles,
+    defaultImg,
   }
 })
