a2d8140777cebbca19cdb34a82cfe66ac53d71c3
亞倫 <tw0905.aaron@gmail.com>
2024-02-20 15:50:32 +0800

    新增 # 購買、購買成功、訂閱成功dialog

---
+++ src/components/modal/content/SubscribeSuccess.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="">
+    <div class="flex flex-col items-center justify-center text-center">
+      <div class="text-base font-normal leading-lg text-gray-57">
+        {{ $t('modal.subscribeSuc.1', { nickname: content?.nickname }) }}
+      </div>
+      <div class="px-75 text-base font-normal leading-lg text-gray-57">
+        {{ $t('modal.subscribeSuc.2') }}
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { storeToRefs } from 'pinia'
+import { useModalStore } from '@/store/modal'
+import { useConfirmData } from '@use/modal/confirm-data'
+
+const { content } = storeToRefs(useModalStore())
+
+useConfirmData(() => {
+  return true
+})
+</script>
