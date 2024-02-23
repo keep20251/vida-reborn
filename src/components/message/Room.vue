7f3d2bf0f00cacaedd08e758ad546ddce7697618
LuLu <hubert.lulululu@gmail.com>
2024-02-23 13:49:05 +0800

    新增 # 私信上滑讀取歷史紀錄

--- src/components/message/Room.vue
+++ src/components/message/Room.vue
@@ -1,7 +1,9 @@
 <template>
   <div v-if="errMsg" class="flex h-full items-center justify-center text-warning">{{ errMsg }}</div>
   <div v-else-if="isClose" class="flex h-full items-center justify-center">X_X!</div>
-  <div v-else-if="loading || isConnecting" class="flex h-full items-center justify-center"><Loading></Loading></div>
+  <div v-else-if="!ready || loading || isConnecting" class="flex h-full items-center justify-center">
+    <Loading></Loading>
+  </div>
   <div v-else-if="user === null" class="flex h-full items-center justify-center text-xl font-bold">
     {{ $t('info.pickMessage') }}
   </div>
@@ -17,7 +19,15 @@
       </div>
     </div>
     <div class="h-1 bg-gray-e5"></div>
-    <div class="scrollbar-md flex grow flex-col space-y-15 overflow-auto md:w-[calc(100%+30px)]">
+    <div
+      class="scrollbar-md flex grow flex-col space-y-15 overflow-x-hidden md:w-[calc(100%+30px)]"
+      ref="messages"
+      @scroll="onScroll"
+    >
+      <div class="flex items-center justify-center py-8 text-gray-a3">
+        <Loading v-if="user.loading"></Loading>
+        <span v-if="user.noMore">{{ $t('common.noMore') }}</span>
+      </div>
       <MessageBox v-for="m in user.messages" :item="m" :key="m.id"></MessageBox>
     </div>
     <div class="flex items-center justify-center space-x-10">
@@ -27,10 +37,19 @@
       <InputWrap v-model="input" class="grow" :appendIconBtn="'sendWhite'" @click:append="send"></InputWrap>
     </div>
   </div>
+  <div
+    v-if="newMsgCount > 0"
+    class="relative -top-66 left-[50%] w-fit -translate-x-[50%] cursor-pointer rounded-md bg-gray-a3 px-12 py-4 text-sm"
+    @click="onNewMsgTipClick"
+  >
+    {{ $t('message.newMessage', { count: newMsgCount }) }}
+  </div>
 </template>
 
 <script setup>
-import { ref, shallowRef, watch } from 'vue'
+import { debounce } from 'lodash'
+import { onBeforeUpdate, onUpdated, ref, shallowRef, watch } from 'vue'
+import { useInfiniteScroll } from '@vueuse/core'
 import { storeToRefs } from 'pinia'
 import { useAccountStore } from '@/store/account'
 import { useChatStore } from '@/store/chat'
@@ -50,7 +69,7 @@ const { isLogin, isCreator } = storeToRefs(accountStore)
 
 const chatStore = useChatStore()
 const { ready } = storeToRefs(chatStore)
-const { getUser } = chatStore
+const { getUser, loadNextHistory } = chatStore
 
 const loading = ref(false)
 const errMsg = ref('')
@@ -75,8 +94,8 @@ watch(
       const newUser = await getUser(uuid)
       if (cleanup) return
       user.value = newUser
-      // checkNewMessage()
-      // loadNextHistory(user.value.id, true)
+      checkNewMessage()
+      loadNextHistory(user.value.uuid)
     } catch (e) {
       errMsg.value = e.message
     } finally {
@@ -101,5 +120,77 @@ function send() {
   } catch (e) {
     console.error(e)
   }
+}
+
+const messages = ref(null)
+const newMsgCount = ref(0)
+useInfiniteScroll(
+  messages,
+  () => {
+    loadNextHistory(props.uuid)
+  },
+  { distance: 100, direction: 'top', interval: 500 },
+)
+
+const onScroll = debounce(({ target: { scrollTop, scrollHeight, clientHeight } }) => {
+  // 將新訊息提示關掉
+  if (scrollHeight - scrollTop - clientHeight < 10) {
+    newMsgCount.value = 0
+  }
+}, 200)
+
+// 確保訊息更新後 scroll container 是保持在原來位置不會亂跳
+let prevMessagesScrollHeight
+let prevMessagesScrollTop
+let atBottom = true
+onBeforeUpdate(() => {
+  if (!messages.value) return
+  prevMessagesScrollHeight = messages.value.scrollHeight
+  prevMessagesScrollTop = messages.value.scrollTop
+
+  // 126 是一個約略估計的高度
+  atBottom = prevMessagesScrollHeight - prevMessagesScrollTop - messages.value.clientHeight < 126
+})
+onUpdated(() => {
+  if (!messages.value) return
+  const isNewMessage = checkNewMessage()
+
+  // 新訊息改變了 scroll container 高度
+  if (isNewMessage) {
+    if (atBottom) {
+      messages.value.scrollTop = messages.value.scrollHeight
+    } else {
+      messages.value.scrollTop = prevMessagesScrollTop
+      newMsgCount.value += 1
+    }
+  }
+
+  // 歷史訊息改變了 scroll container 高度
+  else {
+    const scrollHeightDiff = messages.value.scrollHeight - prevMessagesScrollHeight
+    const newScrollTop = scrollHeightDiff + prevMessagesScrollTop
+    messages.value.scrollTop = newScrollTop
+  }
+})
+function onNewMsgTipClick() {
+  messages.value.scrollTop = messages.value.scrollHeight
+  newMsgCount.value = 0
+}
+
+// 判別添加進來的訊息是否是新訊息
+let prevLastMessageId
+function checkNewMessage() {
+  const lastMessageId =
+    user.value.messages && user.value.messages.length > 0
+      ? user.value.messages[user.value.messages.length - 1].id
+      : undefined
+
+  // 這是新訊息
+  if (lastMessageId !== prevLastMessageId) {
+    prevLastMessageId = lastMessageId
+    return true
+  }
+
+  return false
 }
 </script>
