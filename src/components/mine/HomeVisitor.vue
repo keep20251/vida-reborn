cdd57f6a7140f50d4e7437601a1eedde50768aaa
Zizi <54ziziyo@gmail.com>
2024-03-01 16:42:34 +0800

    調整 # 暫把示意圖拿掉, 內文置中

--- src/components/mine/HomeVisitor.vue
+++ src/components/mine/HomeVisitor.vue
@@ -3,10 +3,7 @@
     <div class="flex items-center justify-center py-20">
       <div class="text-lg font-bold leading-5">{{ $t('info.loginOr') }}{{ $t('info.quickRegister') }}</div>
     </div>
-    <div class="mb-10 text-base font-normal leading-md">{{ $t('content.notLoggedYet') }}</div>
-    <div class="h-[300px] w-[500px] bg-gray-e5">
-      <img class="rounded-md object-cover" src="" alt="" />
-    </div>
+    <div class="mb-10 text-center text-base font-normal leading-md">{{ $t('content.notLoggedYet') }}</div>
     <div class="my-30 flex space-x-10">
       <Button @click="openAuthDialog(AUTH_ROUTES.SIGN_UP)" contrast>{{ $t('title.join') }}</Button>
       <Button @click="openAuthDialog()">{{ $t('title.login') }}</Button>
