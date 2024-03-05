e6988de5404f782d9f7ee99fdfafc72504283d97
Zizi <54ziziyo@gmail.com>
2024-03-05 18:22:24 +0800

    調整 # 購買與收藏帖子列表換成 ArticleList

--- src/pages/mine/MineBuy.vue
+++ src/pages/mine/MineBuy.vue
@@ -9,32 +9,16 @@
     <SubCard></SubCard>
   </div>
   <div v-show="tab === MINE_BUY_TAB.PURCHASED_ARTICLE">
-    <div class="pt-20 text-base font-bold leading-lg">{{ $t('content.allPosts') }} {{ dataExtra?.total }}</div>
-    <div class="overflow-x-hidden">
-      <List :items="dataList" item-key="id" divider>
-        <template #default="{ item }">
-          <Feed class="py-20" :item="item"></Feed>
-        </template>
-        <template #bottom>
-          <div class="flex items-center justify-center py-8 text-gray-a3">
-            <Loading v-if="isLoading">{{ $t('common.loading') }}</Loading>
-            <span v-if="noMore">{{ $t('common.noMore') }}</span>
-          </div>
-        </template>
-      </List>
-    </div>
+    <ArticleList :api="'User.listArticle'" :apiType="GET_ARTICLE_LIST.BOUGHT"></ArticleList>
   </div>
 </template>
 <script setup>
-import { onActivated, onDeactivated, onMounted, onUnmounted, ref } from 'vue'
-import { useFeedStore } from '@/store/feed'
-import { useMineStore } from '@/store/mine'
+import { ref } from 'vue'
 import SubCard from '@comp/card/SubCard.vue'
-import Feed from '@comp/main/Feed.vue'
 import TransactionList from '@comp/mine/TransactionList.vue'
 import Tab from '@comp/navigation/Tab.vue'
-import { useInfinite } from '@use/request/infinite'
 import { GET_ARTICLE_LIST, MINE_BUY_TAB } from '@const'
+import ArticleList from '@/components/mine/ArticleList.vue'
 
 const tab = ref(MINE_BUY_TAB.TRANSACTION)
 const tabOptions = ref([
@@ -42,19 +26,4 @@ const tabOptions = ref([
   { label: 'label.subs', value: MINE_BUY_TAB.SUBSCRIPTION },
   { label: 'label.artPur', value: MINE_BUY_TAB.PURCHASED_ARTICLE },
 ])
-
-const feedStore = useFeedStore()
-const { dataList, dataExtra, isLoading, noMore, init, next, reload } = useInfinite('User.listArticle', {
-  params: { type: GET_ARTICLE_LIST.BOUGHT },
-  transformer: feedStore.sync,
-})
-
-const { setNextFn, clearNextFn } = useMineStore()
-onMounted(() => reload())
-onUnmounted(() => clearNextFn(next))
-onActivated(() => {
-  setNextFn(next)
-  reload()
-})
-onDeactivated(() => clearNextFn(next))
 </script>
