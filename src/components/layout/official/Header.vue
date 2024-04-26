f1941014deddc0fc2bb13f92086b787ed5559ee1
LuLu <hubert.lulululu@gmail.com>
2024-04-26 14:16:53 +0800

    修正 # issue-v1.0.7-官網迭代#1 官網：左上vida logo 導頁至官方主頁。

--- src/components/layout/official/Header.vue
+++ src/components/layout/official/Header.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { useRoute, useRouter } from 'vue-router'
+import { useRouters } from '@use/routers'
 import HeaderBtn from '@/components/layout/official/HeaderBtn.vue'
 import LanguageSelectBtn from '@/components/layout/official/LanguageSelectBtn.vue'
 import MobileMenuBtn from '@/components/layout/official/MobileMenuBtn.vue'
+
+const { to } = useRouters()
 
 const { push } = useRouter()
 
@@ -17,7 +20,12 @@ const headerButtons = [
     class="official-header sticky top-0 z-30 flex h-60 flex-row items-center justify-between gap-62 bg-primary px-15 text-white sm:px-50"
   >
     <div class="sm:flex-grow">
-      <img src="@/assets/images/official/nav-logo.svg?url" class="min-w-[100px] max-w-full object-cover" alt="VIDA" />
+      <img
+        src="@/assets/images/official/nav-logo.svg?url"
+        class="min-w-[100px] max-w-full cursor-pointer object-cover"
+        alt="VIDA"
+        @click="to('home')"
+      />
     </div>
 
     <HeaderBtn
