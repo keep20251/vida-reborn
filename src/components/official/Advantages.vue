6769742611767a9f5a107f19910a0170cfb953af
Zizi <54ziziyo@gmail.com>
2024-05-02 18:24:12 +0800

    修正 # issue-v1.0.11#20 “卡片背景在上下滑動的時候，依據滑動的位置更換 bg-primary

--- src/components/official/Advantages.vue
+++ src/components/official/Advantages.vue
@@ -1,6 +1,10 @@
 <script setup>
-import { ref } from 'vue'
+import { onBeforeUnmount, onMounted, ref } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useAppStore } from '@/store/app'
 import AdvantagesContent from '@comp/official/AdvantagesContent.vue'
+
+const { isMobile } = storeToRefs(useAppStore())
 
 const advantages = ref([
   { id: 1, active: false },
@@ -12,9 +16,39 @@ const advantages = ref([
 const contentClick = (id) => {
   advantages.value = advantages.value.map((advantage) => ({ ...advantage, active: advantage.id === id }))
 }
+
+const option = {
+  threshold: 1,
+  rootMargin: '-225px 0px -225px 0px',
+}
+const callback = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.style.background = '#6567e8'
+      entry.target.style.color = 'white'
+    } else {
+      entry.target.style.background = 'white'
+      entry.target.style.color = 'black'
+    }
+  })
+}
+
+let observer
+onMounted(() => {
+  if (isMobile.value) {
+    observer = new IntersectionObserver(callback, option)
+    const boxes = document.querySelectorAll('.official-advantages-content')
+    boxes.forEach((box) => observer.observe(box))
+  }
+})
+onBeforeUnmount(() => {
+  if (observer) {
+    observer.disconnect()
+  }
+})
 </script>
 <template>
-  <div class="mx-auto my-80 grid grid-cols-1 sm:mx-auto sm:grid-cols-4">
+  <div class="mx-auto my-80 grid grid-cols-1 sm:mx-auto sm:grid-cols-2 md:grid-cols-4">
     <AdvantagesContent
       v-for="advantage in advantages"
       :key="advantage.id"
