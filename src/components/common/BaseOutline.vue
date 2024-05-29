9dccab48651990afc6d3a1e6cd00c96b8b7c117d
亞倫 <tw0905.aaron@gmail.com>
2024-05-29 18:32:53 +0800

    新增 # 通用Tag, TagSwiper 組件、通用Outline樣式組件

---
+++ src/components/common/BaseOutline.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="relative h-35 shrink-0 flex-grow cursor-pointer select-none appearance-none divide-solid rounded-2xl border-gray-cc bg-white px-20 py-12 text-sm font-normal not-italic leading-3 shadow-input outline-none"
+  >
+    <slot></slot>
+  </div>
+</template>
+<script setup></script>
---
+++ src/components/common/Tag.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="cursor-pointer rounded-xl bg-gray-f6 px-18 py-9 text-sm font-normal"
+    @click="$emit('click', value)"
+    :class="{
+      'bg-primary text-white': active,
+    }"
+  >
+    {{ label }}
+  </div>
+</template>
+<script setup>
+defineProps({
+  label: { type: String, default: '' },
+  value: { type: [String, Number], default: '' },
+  active: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+defineEmits(['click'])
+</script>
