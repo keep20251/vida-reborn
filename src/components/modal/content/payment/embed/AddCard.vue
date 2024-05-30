a8476fc3ed2f292d4a4c8b8cf1922e5fc45f46de
亞倫 <tw0905.aaron@gmail.com>
2024-05-30 10:23:58 +0800

    新增 # AddCard 銀行圖示

--- src/components/modal/content/payment/embed/AddCard.vue
+++ src/components/modal/content/payment/embed/AddCard.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex flex-col space-x-10">
-    <div>完全符合支付卡行业数据安全标准</div>
+  <div class="flex flex-col space-y-10">
+    <div class="text-sm font-normal leading-3 text-gray-57">完全符合支付卡行业数据安全标准</div>
     <div>
       <form>
         <div id="stripe-element"></div>
@@ -8,8 +8,28 @@
       </form>
     </div>
     <div></div>
-    <div class="imgs"></div>
-    <div>Ripple Mic Limited, Apartment 206, Jantzen House, Ealing Road, Brentford,England, TW8 0GF</div>
+    <div class="flex flex-row space-x-10">
+      <div v-for="image in images" :key="`credit-card-${image.alt}`">
+        <img :src="image.src" :alt="image.src" />
+      </div>
+    </div>
+    <div class="text-xs font-medium leading-3 text-gray-57">
+      Ripple Mic Limited, Apartment 206, Jantzen House, Ealing Road, Brentford,England, TW8 0GF
+    </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import Discover from '@/assets/images/payment/credit-card/discover.png'
+import JCB from '@/assets/images/payment/credit-card/jcb.png'
+import Maestro from '@/assets/images/payment/credit-card/maestro.png'
+import MasterCard from '@/assets/images/payment/credit-card/master-card.png'
+import Visa from '@/assets/images/payment/credit-card/visa.png'
+
+const images = [
+  { src: Discover, alt: 'Discover' },
+  { src: JCB, alt: 'JCB' },
+  { src: Maestro, alt: 'Maestro' },
+  { src: MasterCard, alt: 'MasterCard' },
+  { src: Visa, alt: 'Visa' },
+]
+</script>
