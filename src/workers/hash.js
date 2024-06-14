e0047db51a5c31db7fae4e2e934f8bc1f0e2ec73
kent <user@userdeMacBook-Pro-2.local>
2024-06-14 14:21:49 +0800

    新增#upload hash 優化

--- src/workers/hash.js
+++ src/workers/hash.js
@@ -1,19 +1,21 @@
-import SparkMD5 from 'spark-md5'
-
 self.onmessage = (evt) => {
   const { slices } = evt.data
-  const spark = new SparkMD5.ArrayBuffer()
+  let totalHash = new ArrayBuffer()
 
   const loadNext = (index) => {
     const reader = new FileReader()
     reader.readAsArrayBuffer(slices[index])
-    reader.onload = (evt) => {
-      spark.append(evt.target.result)
+    reader.onload = async (evt) => {
+      const sliceHash = await crypto.subtle.digest('SHA-256', evt.target.result)
+      totalHash = concatArrayBuffers(totalHash, sliceHash)
       console.log(`hash-${index} complete`)
 
       const nextIndex = index + 1
       if (nextIndex === slices.length) {
-        self.postMessage({ progress: 1, filehash: spark.end() })
+        const hashArrayBuffer = await crypto.subtle.digest('SHA-256', totalHash)
+        const hashBase64 = arrayBufferToBase64(hashArrayBuffer)
+        console.timeEnd('hashing')
+        self.postMessage({ progress: 1, filehash: hashBase64 })
         self.close()
       } else {
         self.postMessage({ progress: nextIndex / slices.length })
@@ -22,5 +24,23 @@ self.onmessage = (evt) => {
     }
   }
 
+  console.time('hashing')
   loadNext(0)
+}
+
+function arrayBufferToBase64(buf) {
+  let binary = ''
+  const bytes = new Uint8Array(buf)
+  const len = bytes.byteLength
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i])
+  }
+  return btoa(binary)
+}
+
+function concatArrayBuffers(a, b) {
+  const c = new Uint8Array(a.byteLength + b.byteLength)
+  c.set(new Uint8Array(a), 0)
+  c.set(new Uint8Array(b), a.byteLength)
+  return c
 }
