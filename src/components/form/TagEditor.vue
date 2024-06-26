3d709614cfed7a020747371c764228c1c8db738c
LuLu <hubert.lulululu@gmail.com>
2024-06-26 12:23:54 +0800

    調整 # issue#v1.2.5#6 【建立帖子】tag增加限制

--- src/components/form/TagEditor.vue
+++ src/components/form/TagEditor.vue
@@ -8,7 +8,9 @@
 
 <script setup>
 import { computed, ref, watch } from 'vue'
+import { useI18n } from 'vue-i18n'
 import { useLocalStorage } from '@vueuse/core'
+import { useModalStore } from '@/store/modal'
 import InputWrap from '@comp/form/InputWrap.vue'
 import OptionsPicker from '@comp/form/OptionsPicker.vue'
 import { LOCAL_STORAGE_KEYS } from '@const'
@@ -31,12 +33,27 @@ const tags = computed({
 })
 const tagOptions = useLocalStorage(LOCAL_STORAGE_KEYS.FEED_TAGS, [])
 
+const { t: $t } = useI18n()
+const { alert } = useModalStore()
+const TAG_COUNT_LIMIT = 10
+const TAG_NAME_LESS = 2
+const TAG_NAME_MORE = 20
 function addTag() {
   if (!tagInput.value) {
     return
   }
 
   const tag = tagInput.value
+
+  // 長度不符合
+  if (stringWidth(tag) < TAG_NAME_LESS) {
+    alert({ title: 'info.publishTagNameLess' })
+    return
+  }
+  if (stringWidth(tag) > TAG_NAME_MORE) {
+    alert({ title: 'info.publishTagNameMore' })
+    return
+  }
 
   // 緩存中已存在
   if (tagOptions.value.some((t) => t.value === tag)) {
@@ -46,7 +63,7 @@ function addTag() {
   }
 
   // 緩存中已滿 10 個
-  if (tagOptions.value.length === 10) {
+  if (tagOptions.value.length === TAG_COUNT_LIMIT) {
     const removedTag = tagOptions.value.pop()
     const tagIndex = tags.value.indexOf(removedTag.value)
     if (tagIndex >= 0) {
@@ -67,6 +84,15 @@ function deleteTag(value) {
   if (tagIndex >= 0) {
     tags.value.splice(tagIndex, 1)
   }
+}
+
+function stringWidth(str) {
+  let width = 0
+  for (let i = 0; i < str.length; i++) {
+    // eslint-disable-next-line no-control-regex
+    width += /[^\x00-\x7F]/.test(str.charAt(i)) ? 2 : 1
+  }
+  return width
 }
 
 watch(
