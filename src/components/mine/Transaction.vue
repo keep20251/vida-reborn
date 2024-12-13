4351f0e5e3b426084e6011bc7a6ebc311f55b674
Zizi <54ziziyo@gmail.com>
2024-12-13 11:50:35 +0800

    修正 # 【交易紀錄】寬度太窄畫面會跑版

--- src/components/mine/Transaction.vue
+++ src/components/mine/Transaction.vue
@@ -3,14 +3,16 @@
     <div class="flex justify-between">
       <div class="flex w-9/12 items-center space-x-5">
         <div class="whitespace-nowrap text-base font-bold leading-md">{{ $t('label.purItems') }}:</div>
-        <div class="text-base font-normal leading-lg text-gray-57">{{ item.item_name }} {{ $t('label.subs') }}</div>
+        <div class="w-8/12 break-words text-base font-normal leading-lg text-gray-57">
+          {{ item.item_name }} {{ $t('label.subs') }}
+        </div>
       </div>
       <div class="w-2/12 text-right text-base font-bold leading-md">$ {{ item.pay_amount }}</div>
     </div>
     <div class="flex items-baseline justify-between space-x-10">
       <div class="flex w-9/12 items-center space-x-5">
         <div class="whitespace-nowrap text-base font-bold leading-md">{{ $t('label.orderNumber') }}:</div>
-        <div class="text-base font-normal leading-lg text-gray-57">{{ item.order_id }}</div>
+        <div class="w-8/12 break-words text-base font-normal leading-lg text-gray-57">{{ item.order_id }}</div>
       </div>
       <div class="w-3/12 flex-wrap text-right text-base font-normal leading-lg text-gray-57">
         {{ tsSecondToYMDhm(item.created_ts, item.created_at) }}
